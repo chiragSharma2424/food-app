2d98376dc9d8e3480c291fb540046052f8574b5f
chirag <sharmachirag242004@gmail.com>
2025-09-07 15:52:51 +0530

    multer package

--- backend/src/controllers/food-controller.js
+++ backend/src/controllers/food-controller.js
@@ -1,7 +1,13 @@
 import foodModel from "../models/food-model.js";
 
 async function createFood(req, res) {
-    
+    console.log(req.foodPartner);
+
+    console.log(req.body);
+
+    console.log(req.file);
+
+    res.send("food item created");
 }
 
 export { createFood }
--- backend/src/routes/food-routes.js
+++ backend/src/routes/food-routes.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import multer from 'multer';
 import { createFood } from '../controllers/food-controller.js';
 import { authFoodPartnerMiddleware } from '../middlewares/auth-middleware.js';
 const foodRouter = express.Router();
 
+// express server cant read any type of file wihch are coming from frontend
+// like img, pdf, video, that's why we need a package called multer it will
+// read the files
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+})
+
 // this api is for creating food item, food item is only be created by food-partner
 // normal user can create this food item so it should be protected
+// /api/food/
 
-foodRouter.post('/', authFoodPartnerMiddleware, createFood);
+foodRouter.post('/', authFoodPartnerMiddleware, upload.single("video"), createFood);
 
 export default foodRouter;
