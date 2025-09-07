e2959e66037eec743b43ce39d0945e5e8d2ca4ee
chirag <sharmachirag242004@gmail.com>
2025-09-07 17:11:10 +0530

    image kit cloud storage setups

--- backend/src/routes/food-routes.js
+++ backend/src/routes/food-routes.js
@@ -8,9 +8,18 @@ const foodRouter = express.Router();
 // like img, pdf, video, that's why we need a package called multer it will
 // read the files
 
+
+// we cant not store files in server we have to store in cloud storage 
+// provider like cloudinary, imgaekit
+// how image kit will work
+// it will store files for us like videos, images, pdf
+// and for that video it will give us that url of that video or file
+
+
 const upload = multer({
     storage: multer.memoryStorage(),
-})
+});
+
 
 // this api is for creating food item, food item is only be created by food-partner
 // normal user can create this food item so it should be protected
---
+++ backend/src/services/storage-services.js
@@ -0,0 +1,7 @@
+import ImageKit from "imagekit";
+
+const imagekit = new ImageKit({
+    publicKey: "",
+    privateKey: "",
+    urlEndpoint
+});
